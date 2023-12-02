b153f887d63d3889caa60341a462998f0f840751
blockarchitech <hello@blockarchitech.com>
2023-12-02 09:37:49 -0500

    Day 2!

---
+++ 2/2a.py
@@ -0,0 +1,46 @@
+lines = open("2/input.txt", "r").readlines()
+
+limits = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
+
+
+get_id = lambda l: int(l.split(":")[0].split(" ")[1])
+
+
+def check_possible(l):
+    id = get_id(l)
+    l = l.split(": ")[1]
+    l = l[:-1]
+    rounds = l.split("; ")
+    for round in rounds:
+        moves = round.split(", ")
+        for move in moves:
+            values = move.split(" ")
+            if int(values[0]) > limits[values[1]]:
+                print(
+                    "Game "
+                    + str(id)
+                    + " is not possible! ("
+                    + str(values[0])
+                    + " > "
+                    + str(limits[values[1]])
+                    + " "
+                    + values[1]
+                    + ")"
+                )
+                return False
+    print("Game " + str(id) + " is possible!")
+    return True
+
+
+values = []
+
+for line in lines:
+    if check_possible(line):
+        values.append(get_id(line))
+
+
+print(sum(values))
---
+++ 2/2b.py
@@ -0,0 +1,33 @@
+lines = open("2/input.txt", "r").readlines()
+
+get_id = lambda l: int(l.split(":")[0].split(" ")[1])
+
+
+def check_possible(l):
+    id = get_id(l)
+    l = l.split(": ")[1]
+    l = l[:-1]
+    rounds = l.split("; ")
+    values = []
+    highest = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+    for round in rounds:
+        round_value = []
+        moves = round.split(", ")
+        for move in moves:
+            values = move.split(" ")
+            round_value.append(int(values[0]))
+            if int(values[0]) > highest[values[1]]:
+                highest[values[1]] = int(values[0])
+    power = lambda h: h["red"] * h["green"] * h["blue"]
+    return {"id": id, "power": power(highest), "highest": highest}
+
+
+powers = []
+for line in lines:
+    powers.append(check_possible(line)["power"])
+
+print(sum(powers))
